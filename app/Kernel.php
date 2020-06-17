2f829c4b2974ae540a46aed2cc08e430641d064d
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-06-17 13:40:37 +0200

    Creates sample of DB connection

--- app/Kernel.php
+++ app/Kernel.php
@@ -6,7 +6,22 @@ namespace App;
 
 class Kernel
 {
+    private $db;
+
     public function run(){
         echo "Boilerplate is ready.\n";
+
+        $this->db = new DB(
+            get_env('SRC_DB_HOST'),
+            get_env('SRC_DB_PORT'),
+            get_env('SRC_DB_USER'),
+            get_env('SRC_DB_PASS'),
+            get_env('SRC_DB_NAME')
+        );
+
+        $this->db->connect();
+        if($this->db->error){
+            die("Database Connection Failed!");
+        }
     }
 }
