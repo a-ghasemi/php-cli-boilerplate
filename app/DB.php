617188d4d45aa9d793e0f4b447e3814b206bf6cd
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-06-17 13:19:25 +0200

    Adds DB Engine

---
+++ app/DB.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App;
+
+
+class DB
+{
+
+    private $connection;
+
+    private $server_name;
+    private $server_port;
+    private $username;
+    private $password;
+    private $db_name;
+
+    public $state;
+    public $error;
+    public $on_demand;
+
+    function __construct($server_name, $server_port, $username, $password, $db_name, $on_demand = false)
+    {
+        $this->server_name = $server_name;
+        $this->server_port = $server_port;
+        $this->on_demand = $on_demand;
+
+        $this->db_name = $db_name;
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->error = null;
+        $this->state = 'created';
+    }
+
+    public function connect(){
+        $this->connection = new \mysqli($this->server_name, $this->username, $this->password);
+
+        if ($this->connection->connect_error) {
+            $this->error = $this->connection->connect_error;
+            $this->state = 'error';
+            return;
+        }
+
+        $this->connection->autocommit(!$this->on_demand);
+        $this->state = 'connected';
+    }
+
+    public function disconnect(){
+        if($this->state !== 'connected') return;
+        $this->connection->close();
+    }
+
+    public function commit(){
+        if($this->on_demand) return true;
+
+        if (!$this->connection->commit()) {
+//            $this->error = $this->connection->connect_error;
+            $this->error = 'Transaction commit failed';
+            $this->state = 'error';
+            return false;
+        }
+
+        return true;
+    }
+
+    public function begin_transaction(){
+        if($this->on_demand) return true;
+
+        if (!$this->connection->begin_transaction()) {
+//            $this->error = $this->connection->connect_error;
+            $this->error = 'Begin Transaction failed';
+            $this->state = 'error';
+            return false;
+        }
+
+        return true;
+    }
+
+    public function rollback(){
+        if($this->on_demand) return false;
+
+        if (!$this->connection->rollback()) {
+//            $this->error = $this->connection->connect_error;
+            $this->error = 'Rollback Transaction failed';
+            $this->state = 'error';
+            return false;
+        }
+
+        return true;
+    }
+
+}
